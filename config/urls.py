230b6918f3c5e7a5ec1c2fd8620068c5bba6c307
Rxdxy <rudyvenegas702@gmail.com>
2026-02-13 09:51:13 -0800

    Set public home to availability + protect calendar with login

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from booking_app.views import availability_dashboard, calendar_dashboard
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -9,8 +11,8 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
-    # Root: send staff (Nazar) straight to calendar
-    path("", RedirectView.as_view(url="/calendar/", permanent=False)),
+    path("", availability_dashboard, name="home"),
+    path("calendar/", login_required(calendar_dashboard), name="calendar_dashboard"),
 
     path("", include("booking_app.urls")),
 ]
