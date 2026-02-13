85ffa17aaf15cbae0e09e3bdedde3242e766e86f
Rxdxy <rudyvenegas702@gmail.com>
2026-02-12 21:21:49 -0800

    Add app login and logout routes

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", RedirectView.as_view(url="/calendar/", permanent=False)),
     path("django-admin/", admin.site.urls),
-    path("", include("booking_app.urls")),
+
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    path("", include("booking_app.urls")),
 ]
