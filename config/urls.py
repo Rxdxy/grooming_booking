725d683f7db6b0055015c0ec6648119a51ab410e
Rxdxy <rudyvenegas702@gmail.com>
2026-02-12 22:20:56 -0800

    Login routing + auth updates

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,13 @@ from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("admin/", RedirectView.as_view(url="/calendar/", permanent=False)),
     path("django-admin/", admin.site.urls),
 
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # Root: send staff (Nazar) straight to calendar
+    path("", RedirectView.as_view(url="/calendar/", permanent=False)),
 
     path("", include("booking_app.urls")),
 ]
