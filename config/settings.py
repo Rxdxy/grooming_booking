84f1c27b2650a46de536012442708065c019e699
Rxdxy <rudyvenegas702@gmail.com>
2026-02-24 12:55:37 -0800

    Fix email host to Outlook SMTP

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ SOFT_GATE_BOOKING = True
 # ===============================
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.office365.com"
+EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 5
