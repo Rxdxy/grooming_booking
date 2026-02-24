481792a9f754b82a3278b31187afb920add50e43
Rxdxy <rudyvenegas702@gmail.com>
2026-02-24 14:18:25 -0800

    Switch email to Resend API

--- config/settings.py
+++ config/settings.py
@@ -144,5 +144,5 @@ SOFT_GATE_BOOKING = True
 #EMAIL_HOST_PASSWORD = "your-email-password-here"
 
 DEFAULT_FROM_EMAIL = "Proxbook <onboarding@resend.dev>"
-RESEND_API_KEY = "re_4BUFDehh_9oVKYuLP6x19k4KjBvWH8sRC"
+RESEND_API_KEY = ""
 resend.api_key = RESEND_API_KEY
