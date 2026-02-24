7e42155a8e418694c256a3549174825845f0f70d
Rxdxy <rudyvenegas702@gmail.com>
2026-02-24 12:17:06 -0800

    Email: add SMTP timeout to prevent worker hang

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.office365.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5
 
 EMAIL_HOST_USER = "proxbook.confirmations@outlook.com"
 EMAIL_HOST_PASSWORD = "Proxbook99."
